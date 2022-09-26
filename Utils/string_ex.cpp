8a52d0647295a0ec933fce7a867a115923172406
Alexey Stupishin <agstup@yandex.ru>
2022-09-26 23:37:23 +0300

    string util

--- Utils/string_ex.cpp
+++ Utils/string_ex.cpp
@@ -11,22 +11,23 @@ size_t str_ex_tolower(char *s)
     std::string ss = s;
     std::string slow = str_ex_tolower(ss);
     size_t length = slow.length();
-    memcpy(s, slow.c_str(), length);
+    memcpy(s, slow.c_str(), length*sizeof(char));
 
     return length;
 }
 
 bool str_ex_compare(char *s1, char *s2, bool case_sens)
 {
+    std::string ss1 = s1, ss2 = s2;
+    int res;
     if (case_sens)
-    {
-        return !strcmp(s1, s2);
-    }
+        res = ss1.compare(ss2);
     else
     {
-        std::string ss1 = s1, ss2 = s2;
         std::string sslowl = str_ex_tolower(ss1);
         std::string sslow2 = str_ex_tolower(ss2);
-        return sslowl.compare(sslow2) == 0;
+        res = sslowl.compare(sslow2);
     }
+
+    return res == 0;
 }
